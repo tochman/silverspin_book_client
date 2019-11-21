472412cbf7118124ff3c495ae10ba10da6b031c8
Thomas <thomas@craftacademy.se>
2019-11-21 08:41:03 +0100

    misc fixes

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const apiUrl = 'http://localhost:3002/books'
 
 const fetchData = async () => {
-  let data = await (await fetch(apiUrl)).json()
+  let data = await (await fetch(apiUrl, { credentials: 'include' })).json()
   return data
+}
+
+const submitData = async () => {
+  let response = await (await fetch('http://localhost:3002/books', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    mode: 'cors',
+    credentials: 'include',
+    body: JSON.stringify({ author: event.target.author.value, title: event.target.title.value })
+  })).json()
+  return response
+}
+
+const submitHandler = async () => {
+  event.preventDefault()
+  submitData()
+    .then((response) => {
+      console.log(response)
+    })
+
 }
 document.addEventListener('DOMContentLoaded', () => {
   let displayElement = document.getElementById('display')
   fetchData().then((books) => {
     books.forEach(book => {
       const showDisplayElement = document.createElement('div')
-      let html = ''
-      
+      let html = `<p>${book.title} by ${book.author}</p>`
+
       showDisplayElement.innerHTML = html
       displayElement.appendChild(showDisplayElement)
     })
